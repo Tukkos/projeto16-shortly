679c8920ecd87f82cc198617acb5a56ebf594f27
Tukkos <jpfcastro@id.uff.br>
2022-10-16 10:07:30 -0300

    refactor: changed /users/me logic and /ranking logic to reduce time complexity

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,27 @@
 import connection from "../database/db.js";
 
 async function getUsersUrls(req, res) {
-    const user = res.locals.user;
+    const session = res.locals.session;
 
     try {
+        const user = await connection.query(`
+        SELECT
+            users.id,
+            users.name,
+            SUM("visitCount") AS "visitCount"
+        FROM users
+        JOIN urls
+            ON users.id = urls."userId"
+        WHERE users.id = $1
+        GROUP BY
+            users.id;
+            `, [session.userId]
+        );
+
+        if(!user) {
+            return res.sendStatus(404);
+        };
+
         const urlsShorten = await connection.query(`
             SELECT 
                 id,
@@ -12,15 +30,13 @@ async function getUsersUrls(req, res) {
                 "visitCount"
             FROM urls
             WHERE "userId" = $1;
-            `, [user.id]
+            `, [session.userId]
         );
 
-        const userVisitCount = urlsShorten.rows.map(counts => counts.visitCount).reduce((a, b) => a + b, 0);
-
         const body = {
-            id: user.id,
-            name: user.name,
-            visitCount: userVisitCount,
+            id: user.rows[0].id,
+            name: user.rows[0].name,
+            visitCount: user.rows[0].visitCount,
             shortenedUrls: urlsShorten.rows
         };
 
@@ -31,41 +47,57 @@ async function getUsersUrls(req, res) {
 };
 
 async function getRanking(req, res) {
-    const ranking = [];
+    // const ranking = [];
 
     try {
-        const users = await connection.query(`
-            SELECT *
-            FROM users;`
+        // const users = await connection.query(`
+        //     SELECT *
+        //     FROM users;`
+        // );
+
+        // for (let i = 0; i < users.rows.length; i++) {
+        //     const urlsShorten = await connection.query(`
+        //         SELECT 
+        //             id,
+        //             "shortUrl",
+        //             url,
+        //             "visitCount"
+        //         FROM urls
+        //         WHERE "userId" = $1;
+        //         `, [users.rows[i].id]
+        //     );
+
+        //     const userVisitCount = urlsShorten.rows.map(counts => counts.visitCount).reduce((a, b) => a + b, 0);
+
+        //     const body = {
+        //         id: users.rows[i].id,
+        //         name: users.rows[i].name,
+        //         linksCount: urlsShorten.rows.length,
+        //         visitCount: userVisitCount
+        //     };
+        //     ranking.push(body);
+        // };
+
+        // ranking.sort((a,b) => a.visitCount < b.visitCount ? 1 : a.visitCount > b.visitCount ? -1 : 0);
+        // const top10 = ranking.slice(0, 10);
+
+        const top10 = await connection.query(`
+            SELECT
+                users.id,
+                users.name,
+                COUNT(urls.id) AS "linksCount",
+                COALESCE(SUM("visitCount"), 0) AS "visitCount"
+            FROM users
+            LEFT JOIN urls
+                ON users.id = urls."userId"
+            GROUP BY
+                users.id
+            ORDER BY
+                "visitCount" DESC
+            LIMIT 10;`
         );
-
-        for (let i = 0; i < users.rows.length; i++) {
-            const urlsShorten = await connection.query(`
-                SELECT 
-                    id,
-                    "shortUrl",
-                    url,
-                    "visitCount"
-                FROM urls
-                WHERE "userId" = $1;
-                `, [users.rows[i].id]
-            );
-
-            const userVisitCount = urlsShorten.rows.map(counts => counts.visitCount).reduce((a, b) => a + b, 0);
-
-            const body = {
-                id: users.rows[i].id,
-                name: users.rows[i].name,
-                linksCount: urlsShorten.rows.length,
-                visitCount: userVisitCount
-            };
-            ranking.push(body);
-        };
-
-        ranking.sort((a,b) => a.visitCount < b.visitCount ? 1 : a.visitCount > b.visitCount ? -1 : 0);
-        const top10 = ranking.slice(0, 10);
         
-        res.status(200).send(top10);
+        res.status(200).send(top10.rows);
     } catch (error) {
         res.status(500).send(error.message);
     }
--- src/userRouter.js
+++ src/userRouter.js
@@ -2,14 +2,12 @@ import express from 'express';
 
 import { getRanking, getUsersUrls } from './controllers/userController.js';
 import { authValidation } from './middlewares/authValidation.js';
-import { getUsersById } from './middlewares/userVallidation.js';
 
 const userRouter = express.Router();
 
 userRouter.get(
     '/users/me',
     authValidation,
-    getUsersById,
     getUsersUrls
 );
 
